7a5ec61acb1b5ab8dd48eeb1e2340ebdf5224815
elizabethkerubo02 <keruboelizabeth02@gmail.com>
2023-03-13 19:02:50 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+  if(feet <= 400){
+    return "This one is on me!";
+  }else if(feet = 2000 && feet <= 2500){
+    return "I will gladly take your thirty bucks.";
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }else{
+    feet > 2500;
+    return "No can do."
+  }
+};
+scuberGreetingForFeet(199);
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return(city === "NYC" )? "Ok, sounds good.": "No go.";
 }
+ternaryCheckCity("NYC");
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+     default:
+      return "Bye."
+
+  }
+
+}
+switchOnCharmFromTip("generous");
